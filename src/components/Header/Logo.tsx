3ac23f8921583842a7de895382a261b601b9efe8
DrabantBY <drabant2022@gmail.com>
2023-01-19 01:01:12 +0300

    feat: add svgr plugin

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,8 +1,10 @@
 import { Link } from 'react-router-dom';
+import { ReactComponent as Louvre } from '../../assets/svg/logo.svg';
 
 export const Logo = () => {
   return (
     <Link className="logo header__logo" to="/">
+      <Louvre />
       LOUVRE
     </Link>
   );
