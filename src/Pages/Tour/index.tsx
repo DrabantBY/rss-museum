5a1c12570299276652e975a511487102e76c78ed
DrabantBY <drabant2022@gmail.com>
2023-02-18 14:27:44 +0300

    feat: add constants

---
+++ src/Pages/Tour/index.tsx
@@ -0,0 +1,14 @@
+const Tour = (): JSX.Element => {
+  return (
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!4v1676718609583!6m8!1m7!1sCAoSLEFGMVFpcE9WeFpRdVN5M0J4OVRfSHBIXzdGdEJIRFRYdkk2U0YtQTEwb2NU!2m2!1d48.86181593314584!2d2.336681797486702!3f81.55!4f-16.599999999999994!5f0.7820865974627469"
+      width="600"
+      height="450"
+      style="border:0;"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"></iframe>
+  );
+};
+
+export default Tour;
